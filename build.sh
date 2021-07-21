66b84240dc124fc2f03c1edc25148f3e6217ef65
edwinjhlee <edwin.jh.lee@gmail.com>
2021-07-21 21:27:09 +0800

    ...

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 prepare(){
     if [ -d code ]; then
         echo "Already exists"
-        git subtree pull --prefix code https://github.com/ogham/exa.git master --squash
+        git subtree pull --prefix code https://github.com/ogham/exa.git v0.10.1 --squash
     else
-        git subtree add --prefix code https://github.com/ogham/exa.git master --squash
+        git subtree add --prefix code https://github.com/ogham/exa.git v0.10.1 --squash
     fi
 }
 
@@ -52,27 +52,14 @@ build_main(){
     }
 }
 
-build_linux_x64(){
-    build_main x86_64-unknown-linux-musl ${appname}.linux.x64
-}
-
-build_linux_arm64(){
-    build_main aarch64-unknown-linux-musl ${appname}.linux.arm64
-}
-
-build_linux_armv7hf(){
-    build_main armv7-unknown-linux-musleabihf ${appname}.linux.armv7hf
-}
-
-build_linux_armv7(){
-    build_main armv7-unknown-linux-musleabi ${appname}.linux.armv7
-}
-
 main(){
-    build_linux_x64
-    build_linux_arm64
-    # build_linux_armv7
-    build_linux_armv7hf
+    build_main x86_64-unknown-linux-musl ${appname}.linux.x64
+
+    # build_main aarch64-unknown-linux-musl ${appname}.linux.arm64
+    build_main aarch64-unknown-linux-gnu ${appname}.linux.arm64
+
+    # build_main armv7-unknown-linux-musleabi ${appname}.linux.armv7
+    build_main armv7-unknown-linux-musleabihf ${appname}.linux.armv7hf
 
     build_main aarch64-apple-darwin ${appname}.darwin.arm64
     build_main x86_64-apple-darwin ${appname}.darwin.x64
